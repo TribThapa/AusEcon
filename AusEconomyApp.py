d346078e523ec043bf02f54dcd7ba2e732d4c3f1
Tribikram Thapa (Rana) <tthapa3@yahoo.com>
2022-10-26 12:30:01 +1100

    Add files via upload

---
+++ AusEconomyApp.py
@@ -0,0 +1,134 @@
+import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+
+# Images
+img = Image.open("MapCash.PNG") # this file must be inside the Streamlit folder directory!
+st.image(img, width=None)
+
+
+st.title("Australian Economy Analysis")
+
+st.write('''
+		 This app determines the Australian economy and gives you a quick overview of its
+		 finanicals and demographics. Play around with the numbers to see how much Australia's GDP would change
+		 by.
+		 ''')
+
+
+# Images
+#img = Image.open("GDP_Bar.PNG") # this file must be inside the Streamlit folder directory!
+#st.image(img, height=100, width=None,caption="Australia's current GDP")
+
+
+
+st.header("Enter your values to predict Australia's GDP")
+
+total_population = st.slider('Population (Example: 7000000)', min_value= 1900000.0, max_value= 50e7, value=25e5, step=1e1)
+total_employed = st.slider('Total employed (in millions)', min_value= 4000.0, max_value= 17e4, value=5e3, step=1e1)
+total_employed_males = st.slider('Total employed males only (in millions)', min_value= 4000.0, max_value= 17e4, value=5e3, step=1e1)
+total_employed_females = st.slider('Total employed females only (in millions)', min_value= 4000.0, max_value= 17e4, value=5e3, step=1e1)
+total_unemployed = st.slider('Total unemployed (in millions)', min_value= 4000.0, max_value= 17e4, value=5e3, step=1e1)
+total_unemployed_males = st.slider('Total unemployed males only (in millions)', min_value= 4000.0, max_value= 17e4, value=5e3, step=1e1)
+total_unemployed_females = st.slider('Total unemployed females only (in millions)', min_value= 4000.0, max_value= 17e4, value=5e3, step=1e1)
+cash_rate_target = st.slider('Cash rate', min_value=0.0, max_value= 20.0, value=2.0, step=0.1)
+interbank_cash_rate = st.slider('Inter bank cash rate', min_value=0.0, max_value= 20.0, value=2.0, step=0.1)
+total_income = st.slider('Total income (in millions)', min_value= 200.0, max_value= 17e6, value=107093.0, step=1e2)
+total_savings = st.slider('Total savings (in millions)', min_value= 200.0, max_value= 17e6, value=313.0, step=1e2)
+
+
+
+user_input = np.array([total_population,
+					   total_employed,
+					   total_employed_males,
+					   total_employed_females,
+					   total_unemployed,
+					   total_unemployed_males,
+					   total_unemployed_females,
+					   cash_rate_target,
+					   interbank_cash_rate,
+					   total_income,
+					   total_savings]).reshape(1, -1)
+
+
+
+
+
+#Load csv files
+gdp = pd.read_csv('GDP.csv')
+gdp_bar = pd.read_csv('GDP.csv')
+cash_rate = pd.read_csv('CashRate.csv')
+population = pd.read_csv('Population.csv')
+employment = pd.read_csv('Employment.csv')
+economy = pd.read_csv('Economy.csv')
+household_income = pd.read_csv('Income.csv')
+gdp_new = gdp.rename(columns={"Unnamed: 0": "Year"})
+
+# Get data to create complete table
+year = population[["Year"]]
+gdp_aus_new = gdp_new[["Australia"]].reset_index(drop=True)
+
+total_population = population[["EstimatedResidentPopulation"]]
+total_employed = employment[["TotalEmployed"]]
+total_employed_males = employment[["TotalEmployed_M"]]
+total_employed_females = employment[["TotalEmployed_F"]]
+total_unemployed = employment[["TotalUnemployed"]]
+total_unemployed_males = employment[["TotalUnemployed_M"]]
+total_unemployed_females = employment[["TotalUnemployed_F"]]
+
+cash_rate_target = cash_rate[["Cash Rate Target"]]
+overnight_cash_rate = cash_rate[["Interbank Overnight Cash Rate"]]
+household_income_total = household_income[["TotalIncome"]]
+household_savings_total = household_income[["TotalSavings"]]
+
+# Create dataframe
+correlation_df = pd.concat([gdp_aus_new,
+                            total_population,
+                            total_employed, 
+                            total_employed_males,
+                            total_employed_females,
+                            total_unemployed,
+                            total_unemployed_males,
+                            total_unemployed_females,
+                            cash_rate_target,
+                            overnight_cash_rate,
+                            household_income_total,
+                            household_savings_total], axis=1)
+
+# Rename to easy convention
+correlation_renamed_df = correlation_df.rename(columns={"Australia": "Aus_GDP",
+                                                        "EstimatedResidentPopulation": "Pop",
+                                                        "TotalEmployed": "Tot Emp",
+                                                        "TotalEmployed_M": "Tot Emp_M",
+                                                        "TotalEmployed_F": "Tot Emp_F",
+                                                        "TotalUnemployed": "Tot Unemp",
+                                                        "TotalUnemployed_M": "Tot Unemp_M",
+                                                        "TotalUnemployed_F": "Tot Unemp_F"})
+
+
+# Define x and ay
+data = correlation_renamed_df.drop(['Aus_GDP'], axis=1)
+target = correlation_renamed_df['Aus_GDP']
+
+# Do train-test split
+data_train, data_test, target_train, target_test = train_test_split(data, 
+                                                                    target,
+                                                                    test_size=0.3,     
+                                                                    random_state=101)
+
+# Build Random Forest regressor
+random_forest = RandomForestRegressor(random_state=101, 
+									  n_estimators=200)
+
+# Build Random Forest model
+random_forest.fit(data_train, target_train)
+
+#making a prediction
+random_forest_pred = random_forest.predict(user_input) 
+
+st.write('The estimated percent change in the Australian GDP is: ', random_forest_pred)
---
+++ README.md
@@ -0,0 +1,113 @@
+# Australian Economy Analysis
+
+## This repo contains all the code and data that were used to analyse the Australian Economy.
+#### Authored by: [Jenny](https://github.com/jennyntd), [Michelle](https://github.com/MishQ666), [Scott](https://github.com/Bomegolf), and [Thapa](https://github.com/TribThapa)
+
+
+Briefly, in this repository you will find our analysis on:
+1. Australia's GDP
+2. Australia's Demographic Data
+3. Australia's Financials
+4. Australia in comparison to countries in the same economic arena 
+<p>&nbsp;</p>
+
+<p align="center">
+    <img src="/Images/MapCash.PNG" width="1500" height="450">
+</p>
+
+<p>&nbsp;</p>
+
+The questions we wanted to answer are:
+
+1. How does Australia's GDP look like when compared to other countries in the same economic arena?
+
+Ans: Australia's GDP looks fairly good when compared to other global markets. This is particularly noticed
+     when you look at 2007-2008 (Global Financial Crisis) and 2020 (Covid). Here, you don't see as much changes to Australia's economy as you see for other countries in the same economic arena (see graph below) suggesting a resilient market to economic stressors . 
+
+<p align="center">
+    <img src="/Images/GDP_Allcountries_bar.png" width="1500" height="250">
+</p>
+
+
+2. What does Australia's demographic data look like?  
+
+Ans: Overall, Australia's population is growing. This is an important factor as population growth plays a
+     key role in the progression and success of a country's economy. Further, another positive trend within the Australian demographic data is the steady growth in the total number of employed females. However, whilst the total number of employed females is increasing, more needs to be done to ensure an equivalent number of employed males and females. 
+
+<p align="center">
+    <img src="/Images/Aus_Demographics_Line.png" width="1500" height="250">
+</p>
+
+
+<p align="center">
+    <img src="/Images/Aus_Demographics_Dropped_Population_Line.png" width="1500" height="250">
+</p>
+
+
+
+3. What does Australia's financials look like?  
+
+Ans: Very similar story to the graphs above - Australia's Gross Income and Total Income seems to be on a 		 steady rise. Whilst this is a good sign, the influence of inflation on Gross Income and Total Income 		 needs to be factored in before any interpretation is extrapolated.   
+
+<p align="center">
+    <img src="/Images/Aus_Financials_Line.png" width="1500" height="250">
+</p>
+
+
+<p align="center">
+    <img src="/Images/Aus_Financials_Dropped_GrossIncome_Line.png" width="1500" height="250">
+</p>
+
+
+
+4. How do we compare to the rest of the world in different sectors of the world economy?  
+
+Ans: Australia seems to be doing slighly well or even above the average when compared to countries
+     with similar economic landscapes. For instance, the rate at which our GDP is growing is good but 
+     not as great as China or India. This, however, could partly be driven by the sheer size of China's and India's population - a key factor in driving a country's economy. When looking into Exported Goods, China clearly leads all countries followed by the United States of America. Here, Australia isn't doing as great as the other countries, and could be something to be focussed on to further improve Australia's economy and GDP. Lastly, inflation seems to effect India the most, this partly could be due to the Rupee being relatively weak to other currencies (e.g., US Dollar, Australian Dollar, Chinese Yen).
+
+<p align="center">
+    <img src="/Images/World_GDP_Growth_Bar_new.png" width="1500" height="250">
+</p>
+
+
+<p align="center">
+    <img src="/Images/World_Goods_Export_Bar_new.png" width="1500" height="250">
+</p>
+
+
+<p align="center">
+    <img src="/Images/World_Inflation_Bar_new.png" width="1500" height="250">
+</p>
+
+
+<p align="center">
+    <img src="/Images/World_Population_Bar_new.png" width="1500" height="250">
+</p>
+
+
+<p align="center">
+    <img src="/Images/ASXvSP500_Bar.png" width="1500" height="250">
+</p>
+
+
+<p>&nbsp;</p>
+
+### The hyperlinks below take you to directories in this repository:
+
+- [App](https://github.com/TribThapa/PropertyAnalysis/tree/main/AusEconomy_App)
+- [Code](https://github.com/TribThapa/PropertyAnalysis/tree/main/Code)
+- [Data](https://github.com/TribThapa/PropertyAnalysis/tree/main/data)
+- [Images](https://github.com/TribThapa/PropertyAnalysis/tree/main/Images)
+- [Presentation](https://github.com/TribThapa/PropertyAnalysis/tree/main/Presentation)
+
+<p>&nbsp;</p>
+
+## Sources
+1. [Australian Bureau of Statistics](https://www.abs.gov.au/)
+2. [World Bank](https://datatopics.worldbank.org/world-development-indicators/)
+3. [CuffLinks](https://github.com/santosjorge/cufflinks)
+4. [PlotLy](https://plotly.com/)
+5. [Machine Learning Classifiers](https://towardsdatascience.com/machine-learning-classifiers-comparison-with-python-33149aecdbca)
+6. [StreamLit](https://streamlit.io/#install)
+
